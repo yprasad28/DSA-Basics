d79396403dcf2686eaa594adb617ce687dd3926b
prasad <crazyprasad28@gmail.com>
2025-05-21 12:17:22 +0530

    updated name

--- Pyramid.java
+++ Pyramid.java
@@ -1,13 +1,13 @@
 package PatternsJava;
 
-public class DiamondReverse {
+public class Pyramid {
     public static void main(String[] args) {
-        int n =5;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
+        int n = 10;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<n-i;j++){
                 System.out.print(" ");
             }
-            for(int k=0;k<n-i;k++){
+            for(int k=0;k<i;k++){
                 System.out.print("* ");
             }
             System.out.println();
--- PyramidReverse.java
+++ PyramidReverse.java
@@ -1,13 +1,13 @@
 package PatternsJava;
 
-public class Diamond {
+public class PyramidReverse {
     public static void main(String[] args) {
-        int n = 10;
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<n-i;j++){
+        int n =5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
                 System.out.print(" ");
             }
-            for(int k=0;k<i;k++){
+            for(int k=0;k<n-i;k++){
                 System.out.print("* ");
             }
             System.out.println();
